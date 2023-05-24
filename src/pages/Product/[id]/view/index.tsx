6957543da126dac1cd86f86e289f30d83724f32f
JR-neto <jorge.demetrio@edu.pucrs.br>
2023-05-24 14:47:18 -0300

    ajuste em detalhe do pedido.

--- src/pages/Product/[id]/view/index.tsx
+++ src/pages/Product/[id]/view/index.tsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { Product } from '../../../../components/ProductCard';
 import styles from './styles.module.scss';
+import { CartContext } from '../../../../contexts/CartContext';
+import { useNavigate } from 'react-router-dom';
 
 const ProductPage = () => {
     const { id } = useParams();
     const [product, setProduct] = useState<Product>()
+    const { addNewProductToCart } = useContext(CartContext);
+    const navigate = useNavigate();
 
     const getProduct = async () => {
         const { data } = await axios.get('https://jersey-market-api-production.up.railway.app/product/list');
@@ -16,11 +20,26 @@ const ProductPage = () => {
         setProduct(filteredProduct[0]);
     };
 
+    const adicionarCarrinho = () => {
+        let produto = { ...product, amount: 1 }
+
+        addNewProductToCart(produto, 'cart');
+        navigate('/cart');
+    }
+
     useEffect(() => {
         getProduct();
     }, []);
     return (
         <div className='ml-4 mt-10 flex flex-row items-center'>
+            <div className='flex flex-col flex-grow items-center w-1/2 h-fit justify-center'>
+                <div className='ml-12 w-96 h-96 rounded-md shadow-md'>
+                    <img src={product?.imageSrc} alt={product?.name} />
+                </div>
+                <div className='pt-4'>
+                    <span >Preço: {product?.price.toLocaleString()}</span>
+                </div>
+            </div>
             <div className='flex flex-col flex-grow items-center'>
                 <span className='text-xl mb-4'>{product?.name}</span >
                 <div className='flex w-1/2 h-fit flex-col items-center space-y-8 shadow-lg border-t-2 bg-zinc-100 rounded-lg p-12'>
@@ -29,7 +48,7 @@ const ProductPage = () => {
                     </div>
                 </div>
                 <div className='mt-4 border bg-green-500 rounded-lg p-2'>
-                    <button>
+                    <button onClick={adicionarCarrinho}>
                         Adicionar ao carrinho
                     </button>
                 </div>
